b87aa9cf08d76069cd30500b62b8f35e236f8382
Myles244 <43722484+Myles244@users.noreply.github.com>
2024-10-08 22:08:57 +0100

    comment plan
    added in comments to plan the milestone project

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 import numpy as np
 import scipy.linalg as la
+
+#for an array of values of k
+
+#a) calculate matrix N
+
+#define wavefunctions phi
+
+#calculate norms and save in matrix N
+
+#b) calculate matices beta sqrt_beta inverse_sqrt_beta Y Y_T
+
+#find eigenvalues and eigen vectors of N
+
+#construct diagonal matrix +-sqrt beta from eigen values of N
+
+#construct Y from eigen vectors of N
+
+#c) calculate matrix H_tilde and then P
+
+#calculate elements of H_tilde
+
+#calculate P using definition
+
+#c) Find the eigenvalues of P
+
+#for the ground state plot the energy against k
