cb84cf8a5117c86b19cdec728d2faaa66921fe55
Myles244 <43722484+Myles244@users.noreply.github.com>
2024-10-19 15:11:29 +0100

    greatly improved integration function

--- main.py
+++ main.py
@@ -2,30 +2,18 @@ import numpy as np
 import scipy.linalg as la
 import plotter
 
-def integrate(function, N, width, min, max):
-	#intergates a real valued function over 4d space using the monte carlo method with N random points
-	#each input dimenstion extends from -width to width
-	#the output dimesnion extends from -max to max
 
-	#genrate N random input vectors
+def integrate(function, N, width):
 	random_inputs=(np.random.random((N,6))-0.5)*2*width
 
-	#generate N random outputs
-	random_outputs=min+(np.random.random((N)))*(max-min)
+	return (64*width**6)*np.sum(function(random_inputs))/N
 
-	#calculate the function at the random inputs and count it if the random point lies inside the function
-	N_inside=np.count_nonzero(function(random_inputs)>random_outputs)
-
-	#return the volume
-	#which is the probability that it was inside the function times the toatl area then adding the offset from the min 
-	return ((N_inside/N)*(max-min)+min)*64*width**6
-
-def acurate_integrate(function, N_integrals, N_points,width, min, max):
+def acurate_integrate(function, N_integrals, N_points,width):
 	#repeatedly do the integral and return the mean and standard error
 
 	values=np.empty((N_integrals))
 	for i in range(N_integrals):
-		values[i]=integrate(function, N_points, width,min, max)
+		values[i]=integrate(function, N_points, width)
 		print(i,values[i],np.mean(values[0:i+1]),np.std(values[0:i+1])/np.sqrt(i))
 	return np.mean(values),np.std(values)/np.sqrt(N_integrals-1)
 
@@ -68,11 +56,9 @@ for n in ns:
 	for n_prime in n_primes:
 		N[n,n_prime]=acurate_integrate(
 			lambda R: phi(ijms[n],k,R)*phi(ijms[n_prime],k,R),
-			N_integrals=10,
+			N_integrals=100,
 			N_points=10000000, #10million is a sensibe max
-			width=3,			
-			min=0,
-			max=1,
+			width=3
 		)
 
 print(N)
