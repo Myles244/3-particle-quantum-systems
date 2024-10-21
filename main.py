b949760ea2795bf39ce50aa2462b1acea434b0f6
Myles244 <43722484+Myles244@users.noreply.github.com>
2024-10-21 10:41:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,92 +1,75 @@
 import numpy as np
 import scipy.linalg as la
-import plotter
+import matplotlib.pyplot as plt
 
-
-def integrate(function, N, width):
-	random_inputs=(np.random.random((N,6))-0.5)*2*width
-
-	return (64*width**6)*np.sum(function(random_inputs))/N
-
-def gaussian_integrate(function, N, stddev):
-
+#monte carlo integrator
+def integrate(function, N, stddev):
+	#using a normal distrobtution in stead of uniform one has many advantages.
+	#It increases the acuracy as the function is being integrated over the domain
+	#It increases the precision since the are more points towards the center where most of the variance of the wavefunction is
 	random_inputs=(np.random.normal(0,stddev,(N,6)))
-
+	
+	#The actual value is calculated as Sum(f(X)/p(X))/N. the 1/p(x) removes the bias on the values of f(x) from the distrobution of p(x)
 	return np.sum(function(random_inputs)*((stddev**6)*8*np.pi**3)*np.exp(((np.linalg.norm(random_inputs,axis=-1)/stddev)**2)/2))/N
 
-def acurate_integrate(function, N_integrals, N_points,width):
-	#repeatedly do the integral and return the mean and standard error
-
+#does multiple integrations and returns the mean and standard error
+def acurate_integrate(function, N_integrals, N_points, stddev):
 	values=np.empty((N_integrals))
 	for i in range(N_integrals):
-		values[i]=integrate(function, N_points, width)
-		print(i,values[i],np.mean(values[0:i+1]),np.std(values[0:i+1])/np.sqrt(i))
-	return np.mean(values),np.std(values)/np.sqrt(N_integrals-1)
-
-def acurate_gaussian_integrate(function, N_integrals, N_points, stddev):
-
-	values=np.empty((N_integrals))
-	for i in range(N_integrals):
-		values[i]=gaussian_integrate(function, N_points, stddev)
+		values[i]=integrate(function, N_points, stddev)
 		#print(i,values[i],np.mean(values[0:i+1]),np.std(values[0:i+1])/np.sqrt(i))
 	return np.mean(values),np.std(values)/np.sqrt(N_integrals-1)
 
-#for an array of values of kappa
-k=1
+def P(functions,hamiltonian):
 
-#a) calculate matrix N
+	dim=np.shape(functions)[0]
+
+	N=np.empty((dim,dim))
+
+	H=np.empty((dim,dim))
+
+	N_points=100000
+	np.random.seed(0)
+	random_inputs=np.random.normal(0,r0,(N_points,6))
+
+	for n in range(dim):
+		for n_prime in range(dim):
+			F1=lambda R:functions[n](R)*functions[n_prime](R)*np.exp(0.5*(np.linalg.norm(R,axis=-1)/r0)**2)
+			N[n,n_prime]=(8*r0**6*np.pi**3)*np.sum(F1(random_inputs))/N_points
+			F2=lambda R:functions[n](R)*hamiltonian(lambda S:functions[n_prime](S),R)*np.exp(0.5*(np.linalg.norm(R,axis=-1)/r0)**2)
+			H[n,n_prime]=(8*r0**6*np.pi**3)*np.sum(F2(random_inputs))/N_points
+			#print("\tN%s%s = <Φ%s|Φ%s> = %s" %(n,n_prime,n,n_prime,N[n,n_prime]))
+
+	#find eigenvalues and eigen vectors of N
+	#print("Finding the eigen values and eigen vectors of N")
+	N_eigenvalues,N_eigenvectors = np.linalg.eig(N)
+
+	#print("Constructing beta and Y")
+	#construct diagonal matrix +-sqrt beta from eigen values of N
+	invs_sqrt_beta=np.zeros((dim,dim))
+	beta=np.zeros((dim,dim))
+	for i in range(dim):
+		invs_sqrt_beta[i,i]=1/(N_eigenvalues[i]**(0.5))
+		beta[i,i]=N_eigenvalues[i]
+
+	#construct Y from eigen vectors of N
+	Y=N_eigenvectors
+
+	#c) calculate matrix H_tilde and then P
+
+	#calculate P using definition
+	#print("Calculating P")
+	return invs_sqrt_beta@np.transpose(Y)@H@Y@invs_sqrt_beta
+
 
 #define wavefunctions phi
-Z=2
-r0=0.52917#anstroms
-ijms=np.array([[0,0,0]])#,[0,0,1],[0,1,0],[1,0,0]])
-dim=np.shape(ijms)[0]
+#R is the combination of both r1 and r2
 def phi(ijm,kappa,R):
 	r1_norm=np.sqrt(R[:,0]**2+R[:,1]**2+R[:,2]**2)
 	r2_norm=np.sqrt(R[:,3]**2+R[:,4]**2+R[:,5]**2)
 	r12_norm=np.sqrt((R[:,0]-R[:,3])**2+(R[:,1]-R[:,4])**2+(R[:,2]-R[:,5])**2)
 	return (r1_norm+r2_norm)**ijm[0]*(r1_norm-r2_norm)**ijm[1]*r12_norm**ijm[2]*np.exp(-Z*(r1_norm+r2_norm)/(kappa*r0))
 
-#calculate the matrix N, where N_nn'=<phi_n|phi_n'>
-
-#start by generating every combination of ns and n_primes in the matrix
-ns=np.arange(0,dim)
-n_primes=ns
-
-#calculate each element of the array by doing the integral associated with each 
-#using a loop here to avoid excessive memory usage since each element requires an integral with 1000000s of paramiters
-print("Calculating elements of N:")
-N=np.empty((dim,dim,2))
-
-for n in ns:
-	for n_prime in n_primes:
-		N[n,n_prime]=acurate_gaussian_integrate(
-			lambda R: phi(ijms[n],k,R)*phi(ijms[n_prime],k,R),
-			N_integrals=10,
-			N_points=100000, #10million is a sensibe max
-			stddev=r0
-		)
-		print("\tN%s%s = <Φ%s|Φ%s> = %s ± %s" %(n,n_prime,n,n_prime,N[n,n_prime,0],N[n,n_prime,1]))
-
-
-#b) calculate matices beta sqrt_beta inverse_sqrt_beta Y Y_T
-
-#find eigenvalues and eigen vectors of N
-print("Finding the eigen values and eigen vectors of N")
-N_eigenvalues,N_eigenvectors = np.linalg.eig(N[:,:,0])
-
-print("Constructing beta and Y")
-#construct diagonal matrix +-sqrt beta from eigen values of N
-invs_sqrt_beta=np.zeros((dim,dim))
-beta=np.zeros((dim,dim))
-for i in range(dim):
-	invs_sqrt_beta[i,i]=1/(N_eigenvalues[i]**(0.5))
-	beta[i,i]=N_eigenvalues[i]
-
-#construct Y from eigen vectors of N
-Y=N_eigenvectors
-
-#c) calculate matrix H_tilde and then P
 
 def laplaccian(function, R, h):
 	return (1/h**2)*(function(R+2*h)-2*function(R+h)+function(R))
@@ -98,27 +81,21 @@ def H_opperator(function, R):
 
 	return laplaccian(function,R,0.00001)-Z/r1_norm-Z/r2_norm+1/r12_norm
 
-#calculate elements of H_tilde
-print("Calculating elements of H tilde:")
-H=np.zeros((dim,dim,2))
-
-for n in ns:
-	for n_prime in n_primes:
-		H[n,n_prime]=acurate_gaussian_integrate(
-			lambda R: phi(ijms[n],k,R)*H_opperator(lambda R_prime:phi(ijms[n_prime],k,R_prime),R),
-			N_integrals=10,
-			N_points=100000, #10million is a sensibe max
-			stddev=r0
-		)
-		print("\tH%s%s = <Φ%s|H|Φ%s> = %s ± %s" %(n,n_prime,n,n_prime,H[n,n_prime,0],H[n,n_prime,1]))
-
-
-#calculate P using definition
-print("Calculating P")
-P=invs_sqrt_beta@np.transpose(Y)@H[:,:,0]@Y@invs_sqrt_beta
-
-#c) Find the eigenvalues of P
-energy_eigenvalues,_=np.linalg.eig(P)
-print(energy_eigenvalues)
-
+#kappa is the maramiter to vary, in order to minimise the energy levels
+kappas=np.linspace(0.5,2,10)
+Z=2#e
+r0=0.52917#anstroms
+ijms=np.array([[0,0,0]])#,[0,0,1],[0,1,0],[1,0,0]]
+dim=np.shape(ijms)[0]
+phis=[]
+E=np.zeros((10,1,1))
+for i in range(100):
+	for j in range(dim):
+		phis.append(lambda R,ijm=ijms[j],kappa=kappas[i]:phi(ijm,kappa,R))
+	print(phis)
+	Eigvalue,_=np.linalg.eig(P(phis,H_opperator))
+	print(Eigvalue)
+E=E.flatten()
+plt.plot(kappas,E)
+plt.show()
 #for the ground state plot the energy against k
