a27381983dad78a96e449d4bebffaac5a7ab4b10
Myles244 <43722484+Myles244@users.noreply.github.com>
2025-02-21 16:23:08 +0000

    Cleaned up

---
+++ exponential.py
@@ -0,0 +1,137 @@
+from header import *
+import scipy.constants as cnst
+
+# --- constants --- 
+
+#masses of nuclear particles:
+        
+#time unit in seconds 
+T_unit=10**(-15)
+L_unit=10**(-12)
+E_unit=cnst.eV
+M_unit=E_unit*T_unit**2/L_unit**2
+
+assert(M_unit==E_unit*T_unit**2/L_unit**2)
+
+#mass of the alpha particle
+m1=(cnst.physical_constants['alpha particle mass'][0])/M_unit
+
+#mass of the muon
+m2=(cnst.physical_constants['muon mass'][0])/M_unit
+
+#mass of the electron
+m3=cnst.m_e/M_unit
+
+m2=m3
+
+#hbar
+hbar=cnst.hbar/(E_unit*T_unit)
+
+#elementry charge
+e=cnst.e
+
+#vacume permiativity
+epsilon0=(cnst.physical_constants['vacuum electric permittivity'][0])*L_unit*E_unit
+
+#prefactor
+def pref(lambdas):
+    return (
+        lambdas[0]/np.sqrt(lambdas[3]*lambdas[6])
+        *lambdas[1]/np.sqrt(lambdas[4]*lambdas[7])
+        *lambdas[2]/np.sqrt(lambdas[5]*lambdas[8])
+        )
+
+#messy norm term
+def mnt(x,y,z): 
+    return (x**2*(y+z)+y**2*(x+z)+z**2*(x+y)+x*y*z)
+
+def I110(lambdas):
+
+    I=(
+        0.5
+        *pref(lambdas)
+        *((2*lambdas[0]**2 + lambdas[0]*lambdas[1] + lambdas[0]*lambdas[2] + lambdas[1]*lambdas[2])
+        /(
+            np.sqrt(mnt(lambdas[3],lambdas[4],lambdas[5]))
+            *np.sqrt(mnt(lambdas[6],lambdas[7],lambdas[8])) 
+        ))
+        )
+    
+    return I
+
+def I111(lambdas):
+
+    I=(
+        pref(lambdas)
+        *(mnt(lambdas[0],lambdas[1],lambdas[2])
+        /(
+            np.sqrt(mnt(lambdas[3],lambdas[4],lambdas[5]))
+            *np.sqrt(mnt(lambdas[6],lambdas[7],lambdas[8])) 
+        ))
+        )
+    return I
+
+def I210(lambdas):
+        
+    I=(
+        pref(lambdas)
+        *((lambdas[0]**2*(3*lambdas[0]+2*lambdas[1]+lambdas[2])+lambdas[1]**2*(lambdas[0]+lambdas[2]) + lambdas[0]*lambdas[1]*lambdas[2])
+        /(
+            np.sqrt(mnt(lambdas[3],lambdas[4],lambdas[5]))
+            *np.sqrt(mnt(lambdas[6],lambdas[7],lambdas[8])) 
+        ))
+        )
+    
+    return I
+
+def I300(lambdas):
+
+    I=(
+        3
+        *pref(lambdas)
+        *(lambdas[0]**2+lambdas[1]**2)/np.sqrt(mnt(lambdas[3],lambdas[4],lambdas[5]))
+        *(lambdas[0]+lambdas[1])/np.sqrt(mnt(lambdas[6],lambdas[7],lambdas[8]))
+        )
+
+    return I
+
+def N_func(i,j,alphas,betas,gammas):
+    lambdas=[
+        1/(np.conjugate(alphas[i])+alphas[j]+np.conjugate(betas[i])+betas[j]),1/(np.conjugate(alphas[i])+alphas[j]+np.conjugate(gammas[i])+gammas[j]),  1/(np.conjugate(betas[i])+betas[j]+np.conjugate(gammas[i])+gammas[j]),  
+        0.5/(np.real(alphas[i])+np.real(betas[i])), 0.5/(np.real(alphas[i])+np.real(gammas[i])), 0.5/(np.real(betas[i])+np.real(gammas[i])),
+         0.5/(np.real(alphas[j])+np.real(betas[j])), 0.5/(np.real(alphas[j])+np.real(gammas[j])), 0.5/(np.real(betas[j])+np.real(gammas[j]))
+        ]
+    I=I111(lambdas)
+    
+    return I
+
+def order(first,second,third,lambdas):
+    return [lambdas[first+second-1],lambdas[first+third-1],lambdas[second+third-1],
+            lambdas[3],lambdas[4],lambdas[5],
+            lambdas[6],lambdas[7],lambdas[8]
+            ]
+
+
+def H_func(i,j,alphas,betas,gammas):
+
+	lambdas=[
+		1/(np.conjugate(alphas[i])+alphas[j]+np.conjugate(betas[i])+betas[j]),1/(np.conjugate(alphas[i])+alphas[j]+np.conjugate(gammas[i])+gammas[j]),  1/(np.conjugate(betas[i])+betas[j]+np.conjugate(gammas[i])+gammas[j]),  
+		0.5/(np.real(alphas[i])+np.real(betas[i])), 0.5/(np.real(alphas[i])+np.real(gammas[i])), 0.5/(np.real(betas[i])+np.real(gammas[i])),
+		0.5/(np.real(alphas[j])+np.real(betas[j])), 0.5/(np.real(alphas[j])+np.real(gammas[j])), 0.5/(np.real(betas[j])+np.real(gammas[j]))
+		]
+
+	T=-0.5*hbar**2*(
+			((alphas[j]**2+betas[j]**2)/m1  +  (alphas[j]**2+gammas[j]**2)/m2  +  (betas[j]**2+gammas[j]**2)/m3)*I111(lambdas)
+			
+			-2*alphas[j]*(1/m1+1/m2)*I110(order(1,2,0,lambdas))
+			-2*betas[j]*(1/m1+1/m3)*I110(order(0,2,1,lambdas))  
+			-2*gammas[j]*(1/m2+1/m3)*I110(lambdas) 
+				
+			+alphas[j]*betas[j]/m1*(I210(order(0,2,1,lambdas))+I210(order(1,2,0,lambdas))-I300(order(2,0,1,lambdas)))
+			+alphas[j]*gammas[j]/m2*(I210(lambdas)+I210(order(2,1,0,lambdas))-I300(order(1,0,2,lambdas)))
+			+betas[j]*gammas[j]/m3*(I210(order(1,0,2,lambdas))+I210(order(2,0,1,lambdas))-I300(lambdas))
+			)
+	
+	V=(e**2/(4*np.pi*epsilon0))*(-2*I110(order(1,2,0,lambdas))-2*I110(order(0,2,1,lambdas))+I110(lambdas))
+
+	return T+V
--- header.py
+++ header.py
@@ -2,10 +2,12 @@ import numpy as np
 
 class Subspace:
 
-  def __init__(self, dim):
+  def __init__(self, dim , verbose=False):
 
     #the dimension of the subspace
     self.dim = dim
+
+    self.verbose=False
 
   def set_N_func(self,f):
     #a funciton that given two indecies and some other paramiters returns the specified element of the N ie the inner product
@@ -22,7 +24,8 @@ class Subspace:
 
   def make_N_mats(self):
     #generate N matrices from N_func
-    print("Constructing the N matrices.")
+    if self.verbose:
+      print("Constructing the N matrices.")
 
     mats=np.empty((self.dim,self.dim,self.num))
 
@@ -34,7 +37,8 @@ class Subspace:
     
   def make_H_mats(self):
     #generate H matrices from H_func
-    print("Constructing the H matrices.")
+    if self.verbose:
+      print("Constructing the H matrices.")
 
     mats=np.empty((self.dim,self.dim,self.num))
 
@@ -46,23 +50,26 @@ class Subspace:
 
   def make_N_mats_vectorized(self):
     #generate N matrices from N_func
-    print("Constructing the N matrices.")
+    if self.verbose:
+      print("Constructing the N matrices.")
     indecies=np.array(np.meshgrid(np.arange(self.dim),np.arange(self.dim),indexing='ij')).transpose([1,2,0])
     self.N_mats=np.transpose(self.N_func(indecies[:,:,0],indecies[:,:,1],*self.params),[2,0,1])
 
   def make_H_mats_vectorized(self):
     #generate H matrices from H_func
-    print("Constructing the H matrices.")
+    if self.verbose:
+      print("Constructing the H matrices.")
     indecies=np.array(np.meshgrid(np.arange(self.dim),np.arange(self.dim),indexing='ij')).transpose([1,2,0])
     self.H_mats=np.transpose(self.H_func(indecies[:,:,0],indecies[:,:,1],*self.params),[2,0,1])
 
   def find_N_eigens(self):
-    print("Finding the eigenvectors and eigenvalues of the N matrices.")
+    if self.verbose:
+      print("Finding the eigenvectors and eigenvalues of the N matrices.")
     self.N_eigenvalues, self.N_eigenvectors=np.linalg.eigh(self.N_mats)
   
   def make_invs_sqrt_beta_mats(self):
-
-    print("Constructing the inverse square root beta matrices.")
+    if self.verbose:
+      print("Constructing the inverse square root beta matrices.")
 
     invs_sqrt_beta=np.zeros((self.num,self.dim,self.dim))
     for i in range(self.dim):
@@ -71,36 +78,36 @@ class Subspace:
     self.invs_sqrt_beta_mats=invs_sqrt_beta
     
   def make_Y_mats(self):
-
-    print("Constructing the Y matrices.")
+    if self.verbose:
+      print("Constructing the Y matrices.")
 
     self.Y=self.N_eigenvectors
 
   def make_P_mats(self):
-
-    print("Constructing the P matrices.")
+    if self.verbose:
+      print("Constructing the P matrices.")
 
     Y_T=np.transpose(self.Y, axes=[0,2,1])
 
     self.P_mats=self.invs_sqrt_beta_mats@Y_T@self.H_mats@self.Y@self.invs_sqrt_beta_mats
 
   def find_P_eigens(self):
-
-    print("Finding P eigenvectors and eigenvalues.")
+    if self.verbose:
+      print("Finding P eigenvectors and eigenvalues.")
 
     self.P_eigenvalues, self.P_eigenvectors=np.linalg.eig(self.P_mats)
 
   def find_energy_levels(self):
-    
-    print("Calculating the energy levels.")
+    if self.verbose:
+      print("Calculating the energy levels.")
 
     energy_levels=np.sort(self.P_eigenvalues,axis=1)
 
     self.energy_levels=energy_levels
 
   def find_energy_eigenstates(self):
-
-    print("Calculating the components of the energy eigenstates.")
+    if self.verbose:
+      print("Calculating the components of the energy eigenstates.")
 
     order=np.argsort(self.P_eigenvalues,axis=1)
 
