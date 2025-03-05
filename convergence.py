d293a6fb07c12b315ebe42b4c4d9d9d66f65425d
Myles244 <43722484+Myles244@users.noreply.github.com>
2025-03-05 15:37:27 +0000

    Made some graphs

--- convergence.py
+++ convergence.py
@@ -1,51 +1,54 @@
 from exponential import *
 from multiprocessing import Pool
 
-N=[]
-E=[]
-D=[]
-params=np.load("data/altaltparams.npy",allow_pickle=True)
+filenames=["data/","data/alt","data/altalt"]
+
+for fn in filenames:
+    N=[]
+    E=[]
+    D=[]
+    params=np.load(fn+"params.npy",allow_pickle=True)
 
 
-def f(i):
-    subspace=Subspace(i)
+    def f(i):
+        subspace=Subspace(i)
 
-    subspace.set_N_func(N_func)
-    subspace.set_H_func(H_func)
+        subspace.set_N_func(N_func)
+        subspace.set_H_func(H_func)
 
-    subspace.set_params(params[:,:i])
+        subspace.set_params(params[:,:i])
 
-    subspace.make_N_mat()
-    subspace.make_H_mat()
-    subspace.find_N_eigens()
-    subspace.make_Y_mat()
-    subspace.make_invs_sqrt_beta_mats()
-    subspace.make_P_mats()
-    subspace.find_P_eigens()
-    subspace.find_energy_levels()
-    subspace.find_energy_eigenstates()
-    expdelta=delta(subspace.energy_eigenstates[0],params[:,:i])
-    print(i)
-    return([i,subspace.energy_levels[0],expdelta])
+        subspace.make_N_mat()
+        subspace.make_H_mat()
+        subspace.find_N_eigens()
+        subspace.make_Y_mat()
+        subspace.make_invs_sqrt_beta_mats()
+        subspace.make_P_mats()
+        subspace.find_P_eigens()
+        subspace.find_energy_levels()
+        subspace.find_energy_eigenstates()
+        expdelta=delta(subspace.energy_eigenstates[0],params[:,:i])
+        print(i)
+        return([i,subspace.energy_levels[0],expdelta])
 
-ilist=range(1,params.shape[1])
+    ilist=range(1,params.shape[1])
 
-data=[]
+    data=[]
 
-if __name__ == '__main__':
-    with Pool() as p:
-        data=(p.map(f, ilist))
-        p.close()
-        p.join()
+    if __name__ == '__main__':
+        with Pool() as p:
+            data=(p.map(f, ilist))
+            p.close()
+            p.join()
 
-        data=np.array(data)
+            data=np.array(data)
 
-        np.save("data/convergence",data,allow_pickle=True)
-
-
+            np.save(fn+"convergence.npy",data,allow_pickle=True)
 
 
-        
+
+
+            
 
 
 
